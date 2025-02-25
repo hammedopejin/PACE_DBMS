f10f05f8733a07b1f66b0815c34607a974414aae
Opejin, Hammed <c-hopejin@pa.gov>
2025-02-25 10:08:50 -0600

    feat: added new column to customer table

--- customer.sql
+++ customer.sql
@@ -1,5 +1,6 @@
 CREATE TABLE customer (
-	customer_id INT PRIMARY KEY,
+	customer_id INT PRIMARY KEY AUTO_INCREMENT,
 	first_name VARCHAR(100),
-	last_name VARCHAR(100)
+	last_name VARCHAR(100),
+    phone VARCHAR(45)
 )
