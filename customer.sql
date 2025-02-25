42e68bd892d3238ecdac3da3ef17cccb2338255d
Hammed Opejin <hammedopejin@yahoo.com>
2025-02-25 10:31:28 -0600

    Update customer.sql
    feat: added new column "email"

--- customer.sql
+++ customer.sql
@@ -2,5 +2,6 @@ CREATE TABLE customer (
 	customer_id INT PRIMARY KEY AUTO_INCREMENT,
 	first_name VARCHAR(100),
 	last_name VARCHAR(100),
-    phone VARCHAR(45)
+    	phone VARCHAR(45),
+	email VARCHAR(55)
 )
